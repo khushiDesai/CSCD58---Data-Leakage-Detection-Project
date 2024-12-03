8eec19714b523ba9dc47789d4edc48ee29d1ecd9
Vicky Chen <vicky20020103@gmail.com>
2024-12-03 17:41:25 -0500

    s

--- src/logger.py
+++ src/logger.py
@@ -12,7 +12,7 @@ ensure_log_directory()
 
 # Configure logging settings
 logging.basicConfig(
-    filename="logs/system.log",
+    filename="src/logs/system.log",
     filemode='w', #overwrite file
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
